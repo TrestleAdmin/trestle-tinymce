7f6076c7f36d3c73869ab65353c6427b37a88062
Sam Pohlenz <sam@sampohlenz.com>
2020-05-06 12:42:20 +0930

    Add Sprockets 4 manifest

--- lib/trestle/tinymce/engine.rb
+++ lib/trestle/tinymce/engine.rb
@@ -3,7 +3,8 @@ require "tinymce/rails"
 module Trestle
   module TinyMCE
     class Engine < Rails::Engine
-      config.assets.precompile << "trestle/tinymce.js"
+      config.assets.precompile << "trestle/tinymce.manifest.js" # Sprockets 4 manifest
+      config.assets.precompile << "trestle/tinymce.js"          # Sprockets 3
     end
   end
 end

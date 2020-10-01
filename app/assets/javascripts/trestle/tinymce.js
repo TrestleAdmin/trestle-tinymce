9c70584843fc03d18103843b0d71c22bdc7e574c
Phuc Nguyen <nguyenhuyphuc0412@gmail.com>
2020-10-01 19:28:44 +0700

    update upload image

--- app/assets/javascripts/trestle/tinymce.js
+++ app/assets/javascripts/trestle/tinymce.js
@@ -8,7 +8,34 @@ Trestle.init(function(e, root) {
     if (editor) { editor.remove(); }
   });
 
-  tinyMCE.init(Trestle.TinyMCE.default);
+  var settingTmp = Trestle.TinyMCE.default;
+  var functionImageUpload = {
+    file_picker_types: 'image',
+    file_picker_callback: function(cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+
+      input.onchange = function() {
+        var file = this.files[0];
+
+        // Note: Now we need to register the blob in TinyMCEs image blob
+        // registry.
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        var blobInfo = blobCache.create(id, file);
+        blobCache.add(blobInfo);
+
+        // Call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+
+      input.click();
+    }
+  }
+  var returnedTarget = Object.assign(settingTmp, functionImageUpload);
+
+  tinyMCE.init(returnedTarget);
 });
 
 // Fix focus issues within Bootstrap modals

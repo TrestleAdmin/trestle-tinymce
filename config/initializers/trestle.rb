deceb7c91275c6b99b8984122039c5e37c5131aa
Phuc Nguyen <nguyenhuyphuc0412@gmail.com>
2020-10-02 11:02:05 +0700

    guide support upload image from computer

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -22,6 +22,30 @@ Trestle.configure do |config|
     ]
     c.link_context_toolbar = true
     c.convert_urls = false
+    c.file_picker_types = 'image'
+    c.file_picker_callback = "function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+
+      input.onchange = function () {
+        var file = this.files[0];
+
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+
+      input.click();
+    }"
   end
 
   config.hook(:javascripts) do

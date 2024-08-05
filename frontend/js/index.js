e15ed2b578101003065fca56bd21a571eb8e1963
Sam Pohlenz <sam@sampohlenz.com>
2024-08-05 21:33:51 +0930

    Add fix to prevent Bootstrap dialog from blocking TinyMCE field focus

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,3 +6,10 @@ import TinyMCEController from './controllers/tinymce_controller'
 Trestle.TinyMCE = {}
 
 Stimulus.register('tinymce', TinyMCEController)
+
+// Prevent Bootstrap dialog from blocking focusin
+document.addEventListener('focusin', (e) => {
+  if (e.target.closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root") !== null) {
+    e.stopImmediatePropagation()
+  }
+})
